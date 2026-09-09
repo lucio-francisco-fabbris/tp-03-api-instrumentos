e7ec090186e7628c120cd0a6d4373e453d0801f8
Lucio Francisco <franfa0610@gmail.com>
2026-09-08 22:12:03 -0300

    feat: implement basic Express server and API endpoints for musical instruments

--- src/index.js
+++ src/index.js
@@ -6,5 +6,40 @@ const rutaDatos = path.join(__dirname, "..", "datos", "instrumentos.json");
 
 async function main () {
     const instrumentos = await leerJSON(rutaDatos);
+
+    const app = express();
+
+    app.use(express.json());
+
+    app.get("/", (req, res) => {
+    res.json({
+        mensaje: "API de instrumentos musicales ya esta disponible"
+        });
+    });
+
+    app.get("/api/instrumentos", (req, res) => {
+        const { familia } = req.query;
+
+        if (!familia) {
+            return res.json(instrumentos);
+        } 
+
+        const resultado = instrumentos.filter(
+            (instrumento) =>
+                instrumento.familia.toLowerCase() === familia.toLowerCase()
+        );
+
+        res.json(resultado);
+        
+    });
+
+    app.listen(3000, () => {
+        console.log("Servidor escuchando en http://localhost:3000");
+    });
+
 }
+
+main();
+
+
 
